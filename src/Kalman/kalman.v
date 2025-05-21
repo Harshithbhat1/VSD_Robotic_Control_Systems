a6ad848d921059040224b5b9254fadbeb57e5f92
Harshith B <151948412+Harshithbhat1@users.noreply.github.com>
2025-05-21 19:16:46 +0530

    Update kalman.v

--- src/Kalman/kalman.v
+++ src/Kalman/kalman.v
@@ -3,21 +3,21 @@ module kalman_filter (
     input wire rst,
     input wire valid,
     input wire [7:0] measurement, 
-    output reg [15:0] filtered_out, 
+    output reg [7:0] filtered_out, 
     output reg ready
 );
     // Constants (Q8.8 fixed point)
-    parameter R = 16'd10240;
-    parameter H = 16'd256;  
-    parameter Q = 16'd2560; 
+    integer R = 40;
+    integer H = 1;  
+    integer Q = 10; 
 
-    reg [15:0] p = 0;        // error covariance
-    reg [15:0] uh = 0;       // estimated value
-    reg [15:0] k;            // Kalman gain
-    reg [15:0] a_q8_8;       
+    real p = 0;        // error covariance
+    real uh = 0;       // estimated value
+    real k=5;           // Kalman gain
+    real a_q8_8;       
 
-    reg [31:0] temp1, temp2, temp3;
-
+    real temp1, temp2, temp3;
+    integer i;
     always @(posedge clk or posedge rst) begin
         if (rst) begin
             p <= 0;
@@ -26,27 +26,29 @@ module kalman_filter (
             ready <= 0;
             filtered_out <= 0;
         end else if (valid) begin
-            a_q8_8 = {measurement, 8'b0}; 
+            a_q8_8 = measurement; 
             temp1 = p * H;                            // p * H
-            temp2 = (H * temp1) >> 8;                // H*p*H in Q8.8
-            temp2 = temp2 + R;                       // H*p*H + R
-            k = temp1 / (temp2 >> 8);                // Q8.8 result
+            temp2 = (H * p*H) ;                // H*p*H in Q8.8
+            temp2 = (H * p * H) + R;                       // H*p*H + R
+           
+           
+            k = p * H / ((H * p * H) + R) ;                // Q8.8 result
 
             temp1 = H * uh;                          // H*uh
             temp2 = a_q8_8 - temp1;                  // a - H*uh
-            temp3 = (k * temp2) >> 8;                // k*(a - H*uh)
-            uh = uh + temp3;
+            temp3 = (k * temp2);                // k*(a - H*uh)
+            uh = uh + (k * (a_q8_8 - (H * uh)));
 
             // p = (1 - k*H)*p + q
-            temp1 = (k * H) >> 8;                    // k*H
-            temp2 = (256 - temp1);                   // 1 - k*H in Q8.8
-            temp3 = (temp2 * p) >> 8;                // (1 - k*H)*p
-            p = temp3 + Q;
-
-            filtered_out <= uh;
-            ready <= 1;
+            temp1 = (k * H) ;                    // k*H
+            temp2 = (1 - temp1);                   // 1 - k*H in Q8.8
+            temp3 = (temp2 * p) ;                // (1 - k*H)*p
+            p = (1-k*H)*p + Q;
+            i=uh;
+            filtered_out = i;
+            ready = 1;
         end else begin
-            ready <= 0;
+            ready = 0;
         end
     end
 
